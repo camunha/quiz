ea3a985332b9df3ca9e756e61c0e5baacf2e7353
camunha <camunha@gmail.com>
2015-08-01 01:00:08 +0200

    Busqueda de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,22 +1,53 @@
 var models = require('../models/models.js');
 
+// Autoload - factoriza el codigo si ruta incluye :quizId
+exports.load = function(req, res, next, quizId){
+	models.Quiz.find(quizId).then(
+		function(quiz){
+			if (quiz){
+				req.quiz = quiz;
+				next();
+			}
+			else {
+				next(new Error('No existe quizId=' + quizId));
+			}
+		}
+	).catch(function(error){ next(error); });
+};
+
+// GET /quizes
+exports.index = function(req, res){
+	if (req.query.search){
+		var filtro = req.query.search;
+		filtro = "%" + filtro.replace(" ", "%") + "%";
+		models.Quiz.findAll({where: ["lower(pregunta) like ?", filtro.toLowerCase()],order:'pregunta ASC'}).then(function(quizes){
+			res.render('quizes/index.ejs', {quizes: quizes});
+		}).catch(function(error){ next(error); });
+	}
+	else{
+		models.Quiz.findAll().then(function(quizes){
+			res.render('quizes/index.ejs', {quizes: quizes});
+		}).catch(function(error){ next(error); });
+	}
+};
+
 // GET /quizes/question
-exports.question = function(req, res){
+exports.show = function(req, res){
 	//res.render('quizes/question', {pregunta: 'Capital de Italia'});
-	models.Quiz.findAll().success(function(quiz){
-		res.render('quizes/question', {pregunta: quiz[0].pregunta});
+	models.Quiz.find(req.params.quizId).then(function(quiz){
+		res.render('quizes/show', {quiz: req.quiz});
 	})
 };
 
 // GET /quizes/answer
 exports.answer = function(req, res){
 	//if (req.query.respuesta === 'Roma'){
-	models.Quiz.findAll().success(function(quiz){
-		if (req.query.respuesta === quiz[0].respuesta){
-			res.render('quizes/answer', {respuesta: 'Correcto'});
+	models.Quiz.find(req.params.quizId).then(function(quiz){
+		if (req.query.respuesta === req.quiz.respuesta){
+			res.render('quizes/answer', {quiz: req.quiz, respuesta: 'Correcto'});
 		}
 		else{
-			res.render('quizes/answer', {respuesta: 'Incorrecto'});
+			res.render('quizes/answer', {quiz: req.quiz, respuesta: 'Incorrecto'});
 		}
 	})
 };
