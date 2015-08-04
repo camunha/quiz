7fff7b91654725e5e5efa17e51d2b30f4d640caa
camunha <camunha@gmail.com>
2015-08-04 09:38:46 +0200

    Autenticacion y sesion

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var partials = require('express-partials');
 var methodOverride = require('method-override');
+var session = require('express-session');
 
 var routes = require('./routes/index');
 
@@ -22,18 +23,31 @@ app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(cookieParser());
+app.use(cookieParser('Quiz 2015'));
+app.use(session());
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+    /*var err = new Error('Not Found');
+    err.status = 404;*/
+
+    if (!req.session.redir){  // si no existe lo inicializa
+        req.session.redir = '/';
+    }
+
+    // guardar path  en session.redir para despues de login
+    if (!req.path.match(/\/login|\/logout/)){
+        req.session.redir = req.path;
+    }
+
+    // hacer visible req.session en las vistas
+    res.locals.session = req.session;
+    next();
 });
+
+app.use('/', routes);
 
 // error handlers
 
